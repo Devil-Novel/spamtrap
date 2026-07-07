b47e8c807e2fe6ada076ec3eaa3dd05f1e518056
Ayman Mostafa <161709364+aimanmoustafa@users.noreply.github.com>
2026-07-07 05:11:01 +0300

    Version1-Update3

--- index.js
+++ index.js
@@ -302,13 +302,14 @@ client.once('ready', async () => {
 
   // Register slash commands
   const { commands } = require('./commands');
-  const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+  const rest = new REST({ version: '10' }).setToken(client.token);
+  const clientId = process.env.CLIENT_ID || client.user.id;
   try {
     if (process.env.GUILD_ID) {
-      await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+      await rest.put(Routes.applicationGuildCommands(clientId, process.env.GUILD_ID), { body: commands });
       console.log(`Commands registered on guild ${process.env.GUILD_ID} (instant)`);
     } else {
-      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+      await rest.put(Routes.applicationCommands(clientId), { body: commands });
       console.log('Commands registered globally (may take up to 1 hour to appear)');
     }
   } catch (err) {
@@ -491,4 +492,4 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN || process.env.DISCORD_TOKEN);
